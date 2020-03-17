86978c7dd4ad6e151718136313d153950ad4bf2c
JCBoyd <jcboyd@pm.me>
2020-03-17 07:51:37 -0600

    Create program shell, commit, and push to remote

---
+++ src/tipcalculator/TipCalculator.java
@@ -0,0 +1,22 @@
+package tipcalculator;
+
+/******************************************************************************
+* TipCalculator.java 
+* Programmer: @author jcboyd
+* Version: 1.0
+* Course: SDEV 2210
+* This program (which calculates the total cost of a meal with various tip 
+* percentages) is designed to practice unit testing and TDD
+******************************************************************************/
+
+
+public class TipCalculator {
+
+    public static void main(String[] args) {
+
+
+
+
+    } //end main
+    
+} //end TipCalculator
